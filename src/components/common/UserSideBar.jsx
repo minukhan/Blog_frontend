9ad4675524b01140f18b7dbb7e1016d83d94b2da
jinii9 <jinii915@naver.com>
2024-10-21 19:50:04 +0900

    fix: package.json 버전 수정(느림 이슈)

--- src/components/common/UserSideBar.jsx
+++ src/components/common/UserSideBar.jsx
@@ -1,42 +1,8 @@
 import React from "react";
 import styled from "styled-components";
-import {
-  Body2,
-  Button,
-  Card,
-  CardAction,
-  CardContent,
-  H5,
-  Subtitle2,
-} from "ui-neumorphism";
 
 function UserSideBar() {
-  return (
-    // <Wrap>
-    <Card>
-      <CardContent>
-        <Subtitle2 secondary style={{ marginBottom: "4px" }}>
-          Word of the day
-        </Subtitle2>
-        <H5>
-          be<span>•</span>nev<span>•</span>o<span>•</span>lent
-        </H5>
-        <Subtitle2 secondary style={{ marginBottom: "12px" }}>
-          adjective
-        </Subtitle2>
-        <Body2>
-          well meaning and kindly.
-          <br />
-        </Body2>
-      </CardContent>
-      <CardAction>
-        <Button text color="var(--primary)">
-          Learn More
-        </Button>
-      </CardAction>
-    </Card>
-    // </Wrap>
-  );
+  return <Wrap>ss</Wrap>;
 }
 
 export default UserSideBar;
