6dfdbf65ff1c2f5728ed9ff82c4c071d4e8bba7b
jinii9 <jinii915@naver.com>
2024-10-24 10:59:34 +0900

    fix: kakaomiddle 에러 해결

--- src/lib/oauth/KakaoMiddle.jsx
+++ src/lib/oauth/KakaoMiddle.jsx
@@ -1,7 +1,6 @@
 import axios from "axios";
 import React, { useEffect } from "react";
 import { useDispatch } from "react-redux";
-import { profileAction } from "../../store/actions/profile";
 
 import { useNavigate } from "react-router-dom";
 
