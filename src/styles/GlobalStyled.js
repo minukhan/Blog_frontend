1f2a308fa86ce1ba7c47fef79875c49027d51913
jinii9 <jinii915@naver.com>
2024-10-25 17:00:41 +0900

    fix: background 색상 수정

--- src/styles/GlobalStyled.js
+++ src/styles/GlobalStyled.js
@@ -18,8 +18,13 @@ export const GlobalStyle = createGlobalStyle`
         --red : #D75858;
         --box-shadow: 8px 8px 20px rgba(174, 174, 192, 0.4),
     -8px -8px 20px rgba(255, 255, 255, 0.7);
-    --border-radius : 16px;
-    --max-width : 1400px;
+        --border-radius : 16px;
+        --max-width : 1400px;
+
+        --primary-color: #f6f7fb;
+        --white-color: #fff;
+        --black-color: #18191a;
+        --red-color: #a0a0a0;
 
     }
     body {
